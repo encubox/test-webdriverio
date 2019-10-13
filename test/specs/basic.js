2b19abbcbf60df18780c6570e3a56b0ddb4c36ac
Stanislav Zaleski <sigmaray@gmail.com>
2019-10-13 19:59:33 +0300

    .

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,23 @@
 const assert = require('assert')
 
 describe('webdriver.io page', () => {
-  it('should have the right title', () => {
-      browser.url('https://webdriver.io')
+  it('does few things', () => {
+      browser.url('http://localhost:3001/')
       const title = browser.getTitle()
-      assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
+      assert.strictEqual(title, 'React App')   
+
+      assert.strictEqual($('#root').getText().includes('...loading dog...'), true);
+
+      browser.waitUntil(() => {
+        return $('#root button').isExisting()
+      }, 5000, 'expected button to show up in 5 seconds');
+
+      $('#root button').click();
+
+      assert.strictEqual($('#root').getText().includes('...loading users...'), true);
+
+      browser.waitUntil(() => {
+        return $('#root').getText().includes('catchPhrase')
+      }, 5000, 'expected to have catchPhrase');      
   })
 })
